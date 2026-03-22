2235f0c386fdbbbfe38f6e8b2ba44a727ab056da
Abderahim20 <abderahimamari066@gmail.com>
2026-03-22 21:02:20 +0100

    this is day 2 we learned variables

---
+++ day 2/main.py
@@ -0,0 +1,26 @@
+#variabale in python
+
+
+first_name = "Abderahim"
+last_name = "Amari"
+country = "Poland"
+city = "Krakow"
+age = 20
+is_married = True
+
+# Printing the values stored in the variables
+
+print("first name:", first_name)
+print("Last name:" , last_name)
+print("country:",country)
+print("city:", city)
+print("age:", age)
+print("married:",is_married)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, city, is_married = "Abderahim", "Amari", "Poland", 20, "Krakow", True
+
+
+print(first_name, last_name, age, is_married)
+print("first Name:", first_name)
