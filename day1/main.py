e4809ec6a33be36c441c4c7d108b8137a14f4764
Abderahim20 <abderahimamari066@gmail.com>
2026-03-22 19:33:30 +0100

    my first python

---
+++ day1/main.py
@@ -0,0 +1,21 @@
+print("hello world ")
+
+
+print(2+3)  # addition(+)
+print(3-1)  # subtraction(-)
+print(2*3)  # multiplication(*)
+print(3**2) # exponentiation(**)
+print(3%2)  # modulo(%)
+print(3//2) # floor division(//)
+print(3/2)  # true division(/
+
+# Checking data types
+
+print(type(10))  # <class 'int'>
+
+print(type(3.14))  # <class 'float'>
+print(type(1+3j))  # <class 'complex'>
+print(type("rahimo"))  # <class 'str'>
+print(type([1,2,3]))  # <class 'list'>
+print(type({"name": "ABDERAHIM"}))  # <class 'dict'>
+print(type({3.14, 4.15}))  # <class 'set'>
---
+++ main.py
@@ -0,0 +1,9 @@
+
+fruits =  {"banana", "mashroom", "appele" , "grape", } 
+
+fruits.remove("appele")
+if "appele" in fruits:
+    print("fuck you")
+
+else :
+    print("fuck nah")
